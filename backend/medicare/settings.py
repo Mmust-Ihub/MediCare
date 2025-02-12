a321d862092292a5dc1a7cb4e4403ea5663de928
sebbievilar2@gmail.com <sebbievilar2@gmail.com>
2025-02-12 04:29:16 +0300

    remove pycache

--- backend/medicare/settings.py
+++ backend/medicare/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'authentication', # custom authentication app
+    'admin', # custom admin app
    
     'rest_framework',
     'phonenumber_field',
